83b2be2ccb81abef72226568faeeef81fd96feb3
BRUGUSOFT <kiran.paturi@brugu.io>
2023-06-26 15:47:54 +0530

    added changes

--- ekart/src/components/Register/index.jsx
+++ ekart/src/components/Register/index.jsx
@@ -1,11 +1,57 @@
 import { useState } from "react"
+import { CountryList } from "../../constants/countrylist";
 
 export const Register = () => {
     const [person, setPersonDetails] = useState({});
 
     const handleInputChanges = (e) => {
-        setPersonDetails({...person,[e.target.name]: e.target.value });
+        setPersonDetails({ ...person, [e.target.name]: e.target.value });
     };
+
+    const bindCountries = () => {
+        return (
+            <div class="row mt-3">
+                <select id="country"
+                    className="form-control"
+                    name="country"
+                    value={person.country}
+                    onChange={(e) => {
+                        handleInputChanges(e);
+                    }}>
+                    {bindOptions(CountryList)}
+                </select>
+            </div>
+        )
+    }
+    const bindOptions = (list) => {
+        return list.map(x => {
+            return (<option value={x.value}>{x.text}</option>)
+        })
+    }
+
+    const bindGender = () => {
+        return (
+            <div class="row mt-3">
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="male" value="M"
+                        checked={person.gender == 'M'} onChange={e => {
+                            handleInputChanges(e)
+                        }} />
+                    <label class="form-check-label" for="male">Male</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="fem" value="F"
+                        checked={person.gender == 'F'}
+                        onChange={e => {
+                            handleInputChanges(e)
+                        }}/>
+                    <label class="form-check-label" for="female">Female</label>
+                </div>
+
+            </div>
+        )
+    }
+
     return (<>
         <div class="row mt-3">
             <input type='text'
@@ -43,6 +89,9 @@ export const Register = () => {
                 }}
             />
         </div>
+        {bindCountries()}
+        {bindGender()}
+
         <h6>{JSON.stringify(person)}</h6>
 
     </>)
---
+++ ekart/src/constants/countrylist.js
@@ -0,0 +1,18 @@
+export const CountryList = [{
+    text: "India",
+    value: "IN"
+},
+{
+    text: "United States",
+    value: "USA"
+},
+{
+    text: "United Kingdom",
+    value: "UK"
+},
+{
+    text: "France",
+    value: "FR"
+}
+
+]
