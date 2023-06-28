fd38099f7bea7258ae7fc787f9929212a681f009
BRUGUSOFT <kiran.paturi@brugu.io>
2023-06-28 11:10:45 +0530

    added changes

--- ekart/src/Pages/Cart.jsx
+++ ekart/src/Pages/Cart.jsx
@@ -1,7 +1,36 @@
-export const Cart=()=>{
-    return(
+import { useEffect, useRef, useState } from "react"
+
+export const Cart = () => {
+    const [inputValue, setInputValue] = useState("");
+    const [xyz,setXyz] = useState('')
+    const inputElement = useRef();
+    const previousInputValue = useRef("");
+    let previousValue=''
+    const focusInput = () => {
+        inputElement.current.focus();
+    }
+    useEffect(() => {
+        previousInputValue.current = inputValue
+    }, [inputValue,xyz])
+    return (
         <div>
-            <h1>Cart</h1>
+             <input type="text"
+                
+                onChange={e => {
+                    setXyz(e.target.value)
+                }}
+            />
+            <input type="text"
+                ref={inputElement}
+                onChange={e => {
+                    setInputValue(e.target.value)
+                }}
+            />
+            <button class="btn btn-primary" onClick={e => {
+                focusInput();
+            }}>Click</button>
+            <h4>Current Value:  {inputValue}</h4>
+            <h4>Previous Value:  {previousValue}</h4>
         </div>
     )
 }
